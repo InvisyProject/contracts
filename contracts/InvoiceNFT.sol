3177c8b6551be2406d90b8e5069db5aa96f38334
0xShikhar <shikhar@iiitmanipur.ac.in>
2024-07-01 11:31:26 +0530

    feat(InvoiceNFT): contract

--- contracts/InvoiceNFT.sol
+++ contracts/InvoiceNFT.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -7,26 +8,81 @@ contract InvoiceNFT is ERC721, Ownable {
     uint256 public tokenIdCounter;
 
     struct Invoice {
-        uint256 amount;
+        uint256 billAmount;
+        string token;
+        uint256 createdAt;
         uint256 dueDate;
-        string payerDetails;
+        string uniqueId;
+        address buyer;
+        address seller;
+        address payer;
+        address payee;
+        string status;
     }
 
     mapping(uint256 => Invoice) public invoices;
 
-    constructor(address initialOwner) ERC721("InvoiceNFT", "INFT") {
-        tokenIdCounter = 0;
-        _mint(initialOwner, tokenIdCounter++);
+    event InvoiceMinted(
+        uint256 tokenId,
+        uint256 billAmount,
+        string token,
+        uint256 createdAt,
+        uint256 dueDate,
+        string uniqueId,
+        address indexed buyer,
+        address indexed seller,
+        address indexed payer,
+        address payee,
+        string status
+    );
+
+    constructor(address initialOwner) ERC721("InvoiceNFT", "INFT")  Ownable(initialOwner) {
     }
 
     function mintInvoice(
         address to,
-        uint256 amount,
+        uint256 billAmount,
+        string memory token,
         uint256 dueDate,
-        string memory payerDetails
+        string memory uniqueId,
+        address buyer,
+        address seller,
+        address payer,
+        address payee,
+        string memory status
     ) public onlyOwner {
         uint256 tokenId = tokenIdCounter++;
         _mint(to, tokenId);
-        invoices[tokenId] = Invoice(amount, dueDate, payerDetails);
+
+        invoices[tokenId] = Invoice({
+            billAmount: billAmount,
+            token: token,
+            createdAt: block.timestamp,
+            dueDate: dueDate,
+            uniqueId: uniqueId,
+            buyer: buyer,
+            seller: seller,
+            payer: payer,
+            payee: payee,
+            status: status
+        });
+
+        emit InvoiceMinted(
+            tokenId,
+            billAmount,
+            token,
+            block.timestamp,
+            dueDate,
+            uniqueId,
+            buyer,
+            seller,
+            payer,
+            payee,
+            status
+        );
+    }
+
+    function getInvoiceDetails(uint256 tokenId) public view returns (Invoice memory) {
+        return invoices[tokenId];
     }
 }
